593a33e723d0e057a8d66430e8af3a03d9b0a2e0
Bhoumic Garg <76861463+codebhomic@users.noreply.github.com>
2021-04-14 11:19:49 +0530

    Update policy.php

--- policy.php
+++ policy.php
@@ -34,7 +34,7 @@
 <h2>Google DoubleClick DART Cookie</h2>
 <p>Google is one of a third-party vendor on our site. It also uses cookies, known as DART cookies, to serve ads to our site visitors based upon their visit to www.website.com and other sites on the internet. However, visitors may choose to decline the use of DART cookies by visiting the Google ad and content network Privacy Policy at the following URL – <a href="https://policies.google.com/technologies/ads">https://policies.google.com/technologies/ads</a></p>
 <h2>Privacy Policies</h2>
-<P>You may consult this list to find the Privacy Policy for each of the advertising partners of <?php echo $siteName ?>. Our Privacy Policy was created with the help of the <a href="http://colearnacademy.com">GDPR Privacy Policy Generator</a></p>
+<P>You may consult this list to find the Privacy Policy for each of the advertising partners of <?php echo $siteName ?>. Our Privacy Policy was created with the help of the <a href="#">GDPR Privacy Policy Generator</a></p>
 <p>Third-party ad servers or ad networks uses technologies like cookies, JavaScript, or Web Beacons that are used in their respective advertisements and links that appear on <?php echo $siteName ?>, which are sent directly to users&#39; browser. They automatically receive your IP address when this occurs. These technologies are used to measure the effectiveness of their advertising campaigns and/or to personalize the advertising content that you see on websites that you visit.</p>
 <p>Note that <?php echo $siteName ?> has no access to or control over these cookies that are used by third-party advertisers.</p>
 <h2>Third Pary Privacy Policies</h2>
